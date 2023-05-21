fb1c90b99ecfcf3e9308d676ad58aee08202ab56
ivamih2k <mihaylov.ivan@siemens.com>
2023-05-21 13:58:30 +0100

    Finish the game

--- JS/index.js
+++ JS/index.js
@@ -1,60 +1,113 @@
-let userPoints = 0, computerPoints = 0;
+const weapons = document.querySelectorAll('.weapon');
+const selectAudio = document.querySelector('#select-weapon-audio');
+const fightAudio = document.querySelector('#fight-audio');
+const fightBtn = document.querySelector('#fight-btn');
+const playerWeaponImg = document.querySelector('#player-weapon');
+const computerWeaponImg = document.querySelector('#computer-weapon');
+let roundDisplay = document.querySelector('#round');
+let scoreDisplay = document.querySelector('#score');
+let roundOutcome = document.querySelector('#round-outcome');
+let playerWeapon = "", computerWeapon = "",
+    playerWeaponImgPath = "", computerWeaponImgPath = "";
+    questionMarkImgPath = "../assets/img/question_mark.jpg"
+let userPoints = 0, computerPoints = 0, round_no = 1;
 
-function getComputerChoice() {
-    let warriors = ["rock", "paper", "scissors"];
-    let choice = Math.floor(Math.random()*2) + 1;
 
-    return warriors[choice];
+function selectWeapon(){
+    selectAudio.currentTime = 0;
+    selectAudio.play();
+
+    if(playerWeapon != ""){
+        let lastSelectedWeapon = document.querySelector(`#${playerWeapon}`);
+        lastSelectedWeapon.classList.remove('weapon-selected');
+    }
+
+    playerWeapon = this.id;
+    this.classList.add('weapon-selected');
+    playerWeaponImgPath = `../assets/img/${playerWeapon}.jpg`
 }
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection == "rock" && computerSelection == "scissors"){
+function getComputerWeapon() {
+    let weapons = ["Rock", "Paper", "Scissors"];
+    let choice = Math.floor(Math.random()*3) + 1;
+    let weapon = weapons[choice-1];
+
+    computerWeaponImgPath = `../assets/img/${weapon}.jpg`
+
+    return weapon;
+}
+
+weapons.forEach(weapon => weapon.addEventListener('click', selectWeapon));
+
+fightBtn.addEventListener('click', function(){
+    playRound(playerWeapon)});
+
+function playRound(playerWeapon){
+    if(playerWeapon=="")
+        return alert("Please, select a weapon.");
+
+    let computerWeapon = getComputerWeapon();
+    let outcome = "";
+
+    fightAudio.currentTime = 0;
+    fightAudio.play();
+
+    playerWeaponImg.src = playerWeaponImgPath;
+    computerWeaponImg.src = computerWeaponImgPath;
+
+    if (playerWeapon == "Rock" && computerWeapon == "Scissors"){
         userPoints++;
-        return `You win! ${playerSelection} beats ${computerSelection}.`;}
-    else if (playerSelection == "rock" && computerSelection == "paper")
+        outcome = `You win! ${playerWeapon} beats ${computerWeapon}.`;}
+    else if (playerWeapon == "Rock" && computerWeapon == "Paper")
     {
         computerPoints++;
-        return `You lose! ${playerSelection} loses to ${computerSelection}.`;}
-    else if (playerSelection == "rock" && computerSelection == "rock")
-    {    return "That's a tie!";}
-    else if (playerSelection == "scissors" && computerSelection == "rock")
+        outcome = `You lose! ${playerWeapon} loses to ${computerWeapon}.`;}
+    else if (playerWeapon == "Rock" && computerWeapon == "Rock")
+    {    outcome = "That's a tie!";}
+    else if (playerWeapon == "Scissors" && computerWeapon == "Rock")
     {    
         computerPoints++;
-        return `You lose! ${playerSelection} loses to ${computerSelection}.`;}
-    else if (playerSelection == "scissors" && computerSelection == "paper")
+        outcome = `You lose! ${playerWeapon} loses to ${computerWeapon}.`;}
+    else if (playerWeapon == "Scissors" && computerWeapon == "Paper")
     {    
         userPoints++;
-        return `You win! ${playerSelection} beats ${computerSelection}.`;}
-    else if (playerSelection == "scissors" && computerSelection == "scissors")
-    {    return "That's a tie!";}
-    else if (playerSelection == "paper" && computerSelection == "rock")
+        outcome = `You win! ${playerWeapon} beats ${computerWeapon}.`;}
+    else if (playerWeapon == "Scissors" && computerWeapon == "Scissors")
+    {    outcome = "That's a tie!";}
+    else if (playerWeapon == "Paper" && computerWeapon == "Rock")
     {     
         userPoints++;
-        return `You win! ${playerSelection} beats ${computerSelection}.`;}
-    else if (playerSelection == "paper" && computerSelection == "scissors")
+        outcome = `You win! ${playerWeapon} beats ${computerWeapon}.`;}
+    else if (playerWeapon == "Paper" && computerWeapon == "Scissors")
     {    
         computerPoints++;
-        return `You lose! ${playerSelection} loses to ${computerSelection}.`;}
-    else if (playerSelection == "paper" && computerSelection == "paper")
-    {    return "That's a tie!";}
+        outcome = `You lose! ${playerWeapon} loses to ${computerWeapon}.`;}
+    else if (playerWeapon == "Paper" && computerWeapon == "Paper")
+    {    outcome = "That's a tie!";}
+
+    round_no++;
+
+    roundOutcome.textContent = outcome;
+    scoreDisplay.textContent = `You : ${userPoints}  -  ${computerPoints} : Computer`;
+    roundDisplay.textContent = `ROUND ${round_no}`;
+
+    if(userPoints == 5)
+        restartGame("User");
+    else if (computerPoints == 5)
+        restartGame("Computer");
 }
 
-function game(){
-    let round_no = 1;
+function restartGame(winner){
+    alert(`${winner} Wins!`);
 
-    for (let i = 0; i < 5; i++)
-    {
-        console.log(`ROUND ${round_no}....`);
-        console.log("FIGHT!");
+    userPoints = 0;
+    computerPoints = 0;
+    round_no = 1;
 
-        // let playerSelection = prompt("Select your warrior (rock, paper or scissors)").toLowerCase();
-        console.log(playRound(playerSelection, getComputerChoice()));
-        console.log(`User: ${userPoints}`);
-        console.log(`Computer: ${computerPoints}`);
-        console.log("");
-        
-        round_no++;
-    }
+    roundOutcome.textContent = "";
+    scoreDisplay.textContent = `You : ${userPoints}  -  ${computerPoints} : Computer`;
+    roundDisplay.textContent = `ROUND 1`;
+
+    playerWeaponImg.src = questionMarkImgPath;
+    computerWeaponImg.src = questionMarkImgPath;
 }
-
-game();
